21f11d8917f6b27d305c5156936b3d246565be5e
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-14 00:54:31 -0500

    Played with some movement numbers for character

--- TileVania/Assets/Scripts/Player.cs
+++ TileVania/Assets/Scripts/Player.cs
@@ -20,6 +20,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     private void Run()
@@ -29,5 +30,19 @@ public class Player : MonoBehaviour
         // create vector2  that will give us the x and y for movement left and right
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+
+    }
+
+
+    private void FlipSprite()
+    {
+        // when absolut value of movement  is greater than zero(mathf.epsilon) our bool will return true
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            // If we are moving then localScale is either +1 or -1 depending on sign of movemtn. 
+            // Mathf.Sign will be positive 1 or negative 1 depending on if the number in it is poistive or negative. So if pos 1 will mean scale x = 1 meaning face right. If neg 1 then scale x = -1 meaning face left
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
     }
 }
