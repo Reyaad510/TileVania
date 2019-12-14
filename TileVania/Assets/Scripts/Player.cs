b4bf77a98473ceffacfe2d145d237b9ff6a1f0d7
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-14 00:33:03 -0500

    Character able to move left and right

---
+++ TileVania/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D myRigidBody;
+
+    [SerializeField] float runSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // cache Rigidbody
+        myRigidBody = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        // get horizontal axis
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 to +1
+        // create vector2  that will give us the x and y for movement left and right
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+}
