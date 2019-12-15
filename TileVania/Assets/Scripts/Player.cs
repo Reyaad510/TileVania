0a2a45de133782c495efa35fb8461ba1fb463c8f
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-15 15:44:17 -0500

    Added Hazards Layer. Added spikes and water as hazards

--- TileVania/Assets/Scripts/Player.cs
+++ TileVania/Assets/Scripts/Player.cs
@@ -100,7 +100,7 @@ public class Player : MonoBehaviour
 
     private void Die()
     {
-        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy")))
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards")))
         {
             isAlive = false;
             myAnimator.SetTrigger("Dying");
