25ca36ed4791922c5e63fedb7134cc0ff2e92e70
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-15 19:19:52 -0500

    Level Exit functioning and incorporated it to have a small slowmo effect when touched

--- TileVania/Assets/Scripts/LevelExit.cs
+++ TileVania/Assets/Scripts/LevelExit.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float waitToLoad = 2f;
+    [SerializeField] float levelExitSlowMotion = 0.2f;
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        StartCoroutine(LoadNextLevel());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadNextLevel()
     {
-        
+        // Smooth slowmo
+        Time.timeScale = levelExitSlowMotion;
+        Time.fixedDeltaTime = 0.02F * Time.timeScale;
+
+        yield return new WaitForSecondsRealtime(waitToLoad);
+        // Slowmo gone when next level starts up
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = 0.02F;
+
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
+
+
+
 }
