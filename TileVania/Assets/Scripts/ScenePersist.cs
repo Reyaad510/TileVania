8103d4cd7f7ce8f45ace369ca24ac98c0e7329e0
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-15 23:20:16 -0500

    Fixed scenepersistence

---
+++ TileVania/Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour
+{
+    int startingSceneIndex;
+
+    private void Awake()
+    {
+        // SingleTon for ScenePersist Game Object
+        int numOfScenePersist = FindObjectsOfType<ScenePersist>().Length;
+        if(numOfScenePersist > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // comparing current scene index to starting scene index. This allows that if player dies that if they collected any coins they will not respawn back. 
+        // Specifically for coins because they are nested in ScenePersist gameobject on unity
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex != startingSceneIndex)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
